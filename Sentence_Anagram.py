badea1bd9b588c81671fbd31fdb026276c3b233f
Sreyansu Sekhar Mohanty <56058574+sreyansu@users.noreply.github.com>
2026-09-01 22:09:33 +0530

    Update Sentence_Anagram.py

--- Sentence_Anagram.py
+++ Sentence_Anagram.py
@@ -1,17 +1,13 @@
 def anagram(s1, s2):
     s1 = s1.replace(" ", "").lower()
     s2 = s2.replace(" ", "").lower()
-    freq1 = {}
-    freq2 = {}
+    if len(s1) != len(s2):
+        return False
+    freq = {}
     for ch in s1:
-        freq1[ch] = freq1.get(ch, 0) + 1
+        freq[ch] = freq.get(ch, 0) + 1
     for ch in s2:
-        freq2[ch] = freq2.get(ch, 0) + 1
-    return freq1 == freq2
-
-s1 = input()
-s2 = input()
-if anagram(s1, s2):
-    print("The sentences are anagrams")
-else:
-    print("Not anagram")
+        freq[ch] = freq.get(ch, 0) - 1
+        if freq[ch] < 0:
+            return False
+    return True
